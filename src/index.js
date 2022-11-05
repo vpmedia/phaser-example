4d2184d7753ce05a65dba86919423838400d509a
andras-macbook-air <andras@vpmedia.hu>
2022-11-05 10:29:46 +0100

    Added eslint and prettier to npm packages

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import { Game, Const } from '@vpmedia/phaser';
+import { Game, Const } from "@vpmedia/phaser";
 
 class GameState {
-    create() {
-        const text = this.game.add.text(320, 240, 'Hello Phaser');
-        text.anchor.set(0.5, 0.5);
-    }
+  create() {
+    const text = this.game.add.text(320, 240, "Hello Phaser");
+    text.anchor.set(0.5, 0.5);
+  }
 }
 
 const config = {
-    width: 640,
-    height: 480,
-    renderer: Const.RENDER_AUTO,
-    backgroundColor: '#333333',
-    disableVisibilityChange: true,
+  width: 640,
+  height: 480,
+  renderer: Const.RENDER_AUTO,
+  backgroundColor: "#333333",
+  disableVisibilityChange: true,
 };
 
 const game = new Game(config);
-game.state.add('Game', GameState);
-game.state.start('Game');
+game.state.add("Game", GameState);
+game.state.start("Game");
 window.game = game;
